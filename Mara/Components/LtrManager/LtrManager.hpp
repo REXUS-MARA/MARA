91c854b758424f014a8bbbffd9b175ae2f27beba
Filip Sowa <thefil9@gmail.com>
2026-03-31 14:58:04 +0200

    added impl files and some boilerplate code

--- Mara/Components/LtrManager/LtrManager.hpp
+++ Mara/Components/LtrManager/LtrManager.hpp
@@ -26,15 +26,68 @@ class LtrManager final : public LtrManagerComponentBase {
 
   private:
     // ----------------------------------------------------------------------
+    // Handler implementations for typed input ports
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for run
+    //!
+    //! Scheduling port for reading from IMU and writing to telemetry
+    //! Stolen from the Mpu, maybe not needed? Idk
+    void run_handler(FwIndexType portNum,  //!< The port number
+                     U32 context           //!< The call order
+                     ) override;
+
+  private:
+    // ----------------------------------------------------------------------
     // Handler implementations for commands
     // ----------------------------------------------------------------------
 
-    //! Handler implementation for command TODO
+    //! Handler implementation for command RESET
     //!
-    //! TODO
-    void TODO_cmdHandler(FwOpcodeType opCode,  //!< The opcode
-                         U32 cmdSeq            //!< The command sequence number
-                         ) override;
+    //! Command to force a RESET
+    void RESET_cmdHandler(FwOpcodeType opCode,  //!< The opcode
+                          U32 cmdSeq            //!< The command sequence number
+                          ) override;
+
+  private:
+    // ----------------------------------------------------------------------
+    // Implementations for internal state machine actions
+    // ----------------------------------------------------------------------
+
+    //! Implementation for action doReset of state machine Mara_I2CSensorStateMachine
+    //!
+    //! Perform reset commands
+    void Mara_I2CSensorStateMachine_action_doReset(SmId smId,                                 //!< The state machine id
+                                                   Mara_I2CSensorStateMachine::Signal signal  //!< The signal
+                                                   ) override;
+
+    //! Implementation for action checkReset of state machine Mara_I2CSensorStateMachine
+    //!
+    //! Check reset took place
+    void Mara_I2CSensorStateMachine_action_checkReset(SmId smId,  //!< The state machine id
+                                                      Mara_I2CSensorStateMachine::Signal signal  //!< The signal
+                                                      ) override;
+
+    //! Implementation for action doEnable of state machine Mara_I2CSensorStateMachine
+    //!
+    //! Perform enable commands
+    void Mara_I2CSensorStateMachine_action_doEnable(SmId smId,                                 //!< The state machine id
+                                                    Mara_I2CSensorStateMachine::Signal signal  //!< The signal
+                                                    ) override;
+
+    //! Implementation for action doConfigure of state machine Mara_I2CSensorStateMachine
+    //!
+    //! Perform configure commands
+    void Mara_I2CSensorStateMachine_action_doConfigure(SmId smId,  //!< The state machine id
+                                                       Mara_I2CSensorStateMachine::Signal signal  //!< The signal
+                                                       ) override;
+
+    //! Implementation for action doRead of state machine Mara_I2CSensorStateMachine
+    //!
+    //! Read the sensor
+    void Mara_I2CSensorStateMachine_action_doRead(SmId smId,                                 //!< The state machine id
+                                                  Mara_I2CSensorStateMachine::Signal signal  //!< The signal
+                                                  ) override;
 };
 
 }  // namespace Mara
---
+++ Mara/Components/LtrManager/LtrTypes.hpp
@@ -0,0 +1,14 @@
+// ======================================================================
+// \title  LtrTypes.hpp
+// \author fsowa
+// \brief  hpp file for defining constants and types for LtrHelpers.cpp
+// ======================================================================
+
+#ifndef Ltr_LtrTypes_HPP
+#define Ltr_LtrTypes_HPP
+#include "Fw/FPrimeBasicTypes.hpp"
+
+namespace Ltr {
+ 
+}
+#endif
