88485df69eb35ace3637b746f296837753bc72a5
Filip Sowa <thefil9@gmail.com>
2026-05-06 12:38:56 +0200

    bumped up the clock to a 100Hz

--- Mara/MaraRPiIP/Top/MaraRPiIPTopology.cpp
+++ Mara/MaraRPiIP/Top/MaraRPiIPTopology.cpp
@@ -19,7 +19,9 @@ namespace Mara {
 Fw::MallocAllocator mallocator;
 
 // The reference topology divides the incoming clock signal (1Hz) into sub-signals: 1Hz, 1/2Hz, and 1/4Hz with 0 offset
-Svc::RateGroupDriver::DividerSet rateGroupDivisorsSet{{{1, 0}, {2, 0}, {4, 0}}};
+// For our current needs, I set up a clock signal of 100Hz (you can change that in the Main.cpp file)
+// That will get spili into sub signals - 1Hz (100 / 100), 1/2hz (100/200), 1/4Hz
+Svc::RateGroupDriver::DividerSet rateGroupDivisorsSet{{{100, 0}, {200, 0}, {400, 0}}};
 
 // Rate groups may supply a context token to each of the attached children whose purpose is set by the project. The
 // reference topology sets each token to zero as these contexts are unused in this project.
